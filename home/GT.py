144a76ecedb46c0a17c4c2a403d50cd75d958849
Indranil <indranilm999@gmail.com>
2018-11-21 14:56:36 +0530

    templates

--- home/GT.py
+++ home/GT.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template
 app= Flask(__name__)
 
+dates = [ {'date' :'November 21 2018'
+    
+    
+    
+        },
+        
+            { 'date' :'November 22 2018'  }
+        
+        
+        ]
 
 @app.route("/")
 def home():    
-    return "Running Successfully"
+    return render_template('home.html', dates = dates)
 
 @app.route("/main")
 def main():
     ret="<head>Main Page</head> <body><h1><b>Inside Body</b></h1></body>"
     return ret
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
