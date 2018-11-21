e12c39f734d46fef9a24b96c91db911d3c7a6795
Indranil <indranilm999@gmail.com>
2018-11-21 13:56:30 +0530

    Added main page

--- home/GT.py
+++ home/GT.py
@@ -5,3 +5,8 @@ app= Flask(__name__)
 @app.route("/")
 def home():    
     return "Running Successfully"
+
+@app.route("/main")
+def main():
+    ret="<head>Main Page</head> <body><h1><b>Inside Body</b></h1></body>"
+    return ret
